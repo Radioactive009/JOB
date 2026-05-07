e655fb60fd41d9794141f1b38596a65448c69b49
Kislay Kumar <kislayreigns20092002@gmail.com>
2026-05-07 11:59:30 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-black text-white h-screen flex items-center justify-center text-4xl">
+      AutoApply AI
+    </div>
+  )
+}
+
+export default App
